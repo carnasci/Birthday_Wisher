a0db094db719d088f8bf32dd4ec4cd3f45746ca1
carnasci <cen.master24@gmail.com>
2025-02-13 18:03:53 -0600

    Day 32 - Cleaned code a little bit after solution, need to practice more dictionary comprehensions with pandas dataframes

--- main.py
+++ main.py
@@ -49,18 +49,13 @@ MY_PASSWORD = "cxauozxphpreajme"
 now = dt.datetime.now()
 today = (now.month, now.day)
 data = pd.read_csv("birthdays.csv")
-
-birthdays_dict = {(data_row.month, data_row.day):data_row for (index, data_row) in data.iterrows()}
-print(birthdays_dict)
-
 letter_choice = randint(1, 3)
+birthdays_dict = {(data_row.month, data_row.day):data_row for (index, data_row) in data.iterrows()}
 
 if today in birthdays_dict:
     with open(f"letter_templates/letter_{letter_choice}.txt", "r") as file:
         letter = file.read()
-        new_letter = letter.replace("[NAME]", birthdays_dict[today]["name"])
-        print(letter)
-        print(new_letter)
+        letter = letter.replace("[NAME]", birthdays_dict[today]["name"])
 
     with smtplib.SMTP(host="smtp.gmail.com") as connection:
         connection.starttls()
@@ -68,5 +63,5 @@ if today in birthdays_dict:
         connection.sendmail(
             from_addr=MY_EMAIL,
             to_addrs=birthdays_dict[today]["email"],
-            msg=f"Subject:Happy Birthday!\n\n{new_letter}"
+            msg=f"Subject:Happy Birthday!\n\n{letter}"
         )
